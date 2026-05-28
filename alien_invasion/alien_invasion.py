2d523c3956d80ddaac30c8bc23f177df81e84ba7
SevenWoos <swenwu96@gmail.com>
2026-05-28 16:49:55 -0700

    Checked for aliens hit edges and move the fleet down.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -36,7 +36,7 @@ class AlienInvasion:
     while True:
       self._check_events()
       self.ship.update()
-      self.aliens.update()
+      self._update_aliens()
       self._update_bullets()
       self._update_screen()
       self.clock.tick(60)
@@ -82,6 +82,11 @@ class AlienInvasion:
       if bullet.rect.bottom <= self.screen_rect.top:
         self.bullets.remove(bullet)
         
+  def _update_aliens(self):
+    """Check if the fleet is at an edge, then update the positions of all aliens in the fleet."""
+    self._check_fleet_edges()
+    self.aliens.update()
+        
   def _create_alien(self, x_position, y_position):
     """Create an alien and place it in the row."""
     new_alien = Alien(self)
@@ -107,6 +112,19 @@ class AlienInvasion:
       # Finished a row; reset x-value, and increment y-value to next row.
       current_x = alien_width
       current_y += 2 * alien_height
+  
+  def _check_fleet_edges(self):
+    """Respond appropriately if any aliens have reached an edge."""
+    for alien in self.aliens.sprites():
+      if alien.check_edges():
+        self._change_fleet_direction()
+        break
+  
+  def _change_fleet_direction(self):
+    """Drop the entire fleet and change the fleet's direction."""
+    for alien in self.aliens.sprites():
+      alien.rect.y += self.settings.fleet_drop_speed
+    self.settings.fleet_direction *= -1
   
   def _update_screen(self):
     """Update images on the screen, and flip to the new screen."""
