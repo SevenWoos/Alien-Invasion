1cc64cc32246c0d9e70a56940dd476be62e6d8c6
SevenWoos <swenwu96@gmail.com>
2026-05-28 16:55:32 -0700

    Check for bullet and alien ship collisions. Handle collisions.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -82,6 +82,22 @@ class AlienInvasion:
       if bullet.rect.bottom <= self.screen_rect.top:
         self.bullets.remove(bullet)
         
+    # Check for any bullets that have hit aliens.
+    # if so, get rid of the bullet and the alien.
+    self._check_bullet_alien_collisions()
+  
+  def _check_bullet_alien_collisions(self):
+    """Respond to bullet-alien collisions."""
+    # Remove any bullets and aliens that have collided.
+    collisions = pygame.sprite.groupcollide(
+      self.bullets, self.aliens, True, True
+    )
+    # Respawn new fleet when one is destroyed.
+    if not self.aliens:
+      # Destroy existing bullet sprites and creates new fleet.
+      self.bullets.empty()
+      self._create_fleet()
+        
   def _update_aliens(self):
     """Check if the fleet is at an edge, then update the positions of all aliens in the fleet."""
     self._check_fleet_edges()
