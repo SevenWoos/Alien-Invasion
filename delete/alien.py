28a7d5621fb27c34dd1b435078ded256a98af88c
SevenWoos <swenwu96@gmail.com>
2026-05-31 10:18:14 -0700

    Updated the check_edges function in Alien class for test folder.

--- delete/alien.py
+++ delete/alien.py
@@ -19,7 +19,7 @@ class Alien(Sprite):
   def check_edges(self):
     """Return true if alien is at edge of screen."""
     screen_rect = self.screen.get_rect()
-    return (self.rect.right >= self.screen.right) or (self.rect.left <= 0)
+    return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
     
   def update(self):
     self.x += self.settings.alien_speed * self.settings.fleet_direction
