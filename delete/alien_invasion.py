53739850d36737ad456e315cc44c1e2d5232b092
SevenWoos <swenwu96@gmail.com>
2026-05-31 13:01:30 -0700

    Check for bullet and alien ship collisions. Handle collisions for test folder.

--- delete/alien_invasion.py
+++ delete/alien_invasion.py
@@ -71,6 +71,19 @@ class AlienInvasion:
     for bullet in self.bullets.copy():
       if bullet.rect.bottom <= self.screen_rect.top:
         self.bullets.remove(bullet)
+    
+    # Check for any bullets that have hit aliens.
+    # if so, get rid of the bullet and the alien.
+    self._check_bullet_alien_collisions()
+    
+  def _check_bullet_alien_collisions(self):
+    collisions = pygame.sprite.groupcollide(
+      self.bullets, self.aliens, True, True
+    )
+    # Respawn new fleet when one is destroyed.
+    if not self.aliens:
+      self.bullets.empty()
+      self._create_fleet()
         
   def _update_aliens(self):
     self._check_fleet_edges()
--- delete/settings.py
+++ delete/settings.py
@@ -17,5 +17,5 @@ class Settings:
     
     # Alien Settings.
     self.alien_speed = 1.0
-    self.fleet_drop_speed = 100
+    self.fleet_drop_speed = 50
     self.fleet_direction = -1
