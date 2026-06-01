18a1267e1b3ae137fc6f4d29df1dd0cc6861ec6d
SevenWoos <swenwu96@gmail.com>
2026-05-31 18:39:09 -0700

    Created bullet class and ability fire them from ship for sideways shooter.

--- sideways_shooter/sideways_shooter.py
+++ sideways_shooter/sideways_shooter.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class SidewaysShooter:
   """Overall class to manage game assets and behavior."""
@@ -15,15 +16,20 @@ class SidewaysShooter:
     self.settings = Settings()
     
     self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+    self.screen_rect = self.screen.get_rect()
     pygame.display.set_caption("Sideways Shooter")
     
     # Screen must be defined BEFORE ship, since we're accessing it.
     self.ship = Ship(self)
     
+    # Group that holds the bullets, and allows you to manage the bullets fired from the ship.
+    self.bullets = pygame.sprite.Group()
+    
   def run_game(self):
     while True:
       self._check_events()
       self.ship.update()
+      self._update_bullet()
       self._update_screen()
       self.clock.tick(60)
       
@@ -44,16 +50,36 @@ class SidewaysShooter:
       self.ship.moving_up = True
     elif event.key == pygame.K_DOWN:
       self.ship.moving_down = True
+    elif event.key == pygame.K_SPACE:
+      self._fire_bullet()
   
   def _check_events_keyup(self, event):
     if event.key == pygame.K_UP:
       self.ship.moving_up = False
     elif event.key == pygame.K_DOWN:
       self.ship.moving_down = False
+      
+  def _fire_bullet(self):
+    """Create a new bullet and add it to the bullets group."""
+    if len(self.bullets) < self.settings.bullets_allowed:
+      new_bullet = Bullet(self)
+      self.bullets.add(new_bullet)
+  
+  def _update_bullet(self):
+    """Update position of bullets and get rid of old bullets."""
+    # Apply update() method on all bullets to update their positions.
+    self.bullets.update()
+    # Get rid of old bullets that have disappeared.
+    for bullet in self.bullets.copy():
+      if bullet.rect.left >= self.screen_rect.right:
+        self.bullets.remove(bullet)
   
   def _update_screen(self):
     """Update images on the screen, and flip to the new screen."""
     self.screen.fill(self.settings.bg_color)
+    
+    for bullet in self.bullets.sprites():
+      bullet.draw_bullet()
     
     # Redraw the ship at its current location.
     self.ship.blitme()
